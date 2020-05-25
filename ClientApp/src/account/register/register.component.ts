7d6956ff23852fb9d5198eff028ffb56fbebbd8b
Ravi Mittal <ravimittal16@gmail.com>
2020-05-25 13:43:07 +0530

    updated route

--- ClientApp/src/account/register/register.component.ts
+++ ClientApp/src/account/register/register.component.ts
@@ -1,27 +1,33 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { FormGroup, Validators, FormBuilder } from "@angular/forms";
 
 import { accountModuleAnimation } from "@shared/animations/routerTransition";
 import { RegisterModel } from "@shared/service-proxies/service-proxies";
-
-import { FormGroup, Validators, FormBuilder } from "@angular/forms";
-
-import { Observable } from "rxjs";
 import { RegsiterFacade } from "@core-data/index";
+
+import { Observable, Subscription } from "rxjs";
 
 @Component({
   templateUrl: "./register.component.html",
   animations: [accountModuleAnimation()],
   styleUrls: ["./register.component.less"],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
   model: RegisterModel = new RegisterModel();
   registerForm: FormGroup;
+  externalSignupId: String;
+  routeSub: Subscription;
   errors$: Observable<string[]>;
   uiState$: Observable<boolean>;
   constructor(
     private _fb: FormBuilder,
+    private _route: ActivatedRoute,
     private _registerFacade: RegsiterFacade
-  ) {}
+  ) {
+    this.errors$ = this._registerFacade.errors$;
+    this.uiState$ = this._registerFacade.uiState$;
+  }
 
   register(isValid): void {
     if (isValid) {
@@ -42,11 +48,19 @@ export class RegisterComponent implements OnInit {
       privatePolicyCheck: [false],
     });
   }
-  save(): void {}
+
+  private routeParamsForExternalSignup() {
+    this.routeSub = this._route.queryParams.subscribe((params) => {
+      this.externalSignupId = params["exsignup"] || "";
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
+  }
 
   ngOnInit(): void {
     this._createRegisterForm();
-    this.errors$ = this._registerFacade.errors$;
-    this.uiState$ = this._registerFacade.uiState$;
+    this.routeParamsForExternalSignup();
   }
 }
