8870676a28d4ad3c34e1ad0616ddc776d8851a88
Ravi Mittal <ravimittal16@gmail.com>
2020-07-02 00:04:45 +0530

    updated error-view component
    updated error-view component

--- ClientApp/src/shared/error-view/error.view.component.ts
+++ ClientApp/src/shared/error-view/error.view.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit, Input, ChangeDetectorRef } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectorRef,
+  OnDestroy,
+} from "@angular/core";
 import { Observable } from "rxjs";
+import { SubSink } from "subsink";
 
 @Component({
   selector: "app-error-view",
   templateUrl: "./error.view.component.html",
   styleUrls: ["./error.view.component.scss"],
 })
-export class AppErrorViewComponent implements OnInit {
+export class AppErrorViewComponent implements OnInit, OnDestroy {
   @Input() errors$: Observable<string[]>;
+  @Input() observableDefined: boolean = false;
   errors: string[] = [];
+  private _sink = new SubSink();
   constructor(private _cdr: ChangeDetectorRef) {}
+  ngOnDestroy(): void {
+    this._sink.unsubscribe();
+  }
 
   ngOnInit() {
-    this.errors$.subscribe((errors) => {
-      this.errors.push(...errors);
-      this._cdr.detectChanges();
-    });
+    if (this.observableDefined) {
+      this._sink.add(
+        this.errors$.subscribe((errors) => {
+          this.errors.push(...errors);
+          this._cdr.detectChanges();
+        })
+      );
+    }
   }
 }
