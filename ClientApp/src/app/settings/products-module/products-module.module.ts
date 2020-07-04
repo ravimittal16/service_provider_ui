a4a980716cc8b3c90635b097c9cbf1a92fae7d25
Ravi Mittal <ravimittal16@gmail.com>
2020-07-04 14:13:27 +0530

    Adding Routes for Products
    Adding Routes for Products

--- ClientApp/src/app/settings/products-module/products-module.module.ts
+++ ClientApp/src/app/settings/products-module/products-module.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Routes } from "@angular/router";
+
+const routes: Routes = [{ path: "list" }, { path: "prdcts-cuspri" }];
 
 @NgModule({
   declarations: [],
