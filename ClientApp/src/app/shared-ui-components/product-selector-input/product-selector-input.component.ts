517c4cd52098b13089fdac5134304f2fd6bd7ad4
Ravi Mittal <ravimittal16@gmail.com>
2020-10-28 10:27:04 +0530

    created customer selector component (typehead)
    created customer selector component (typehead)

---
+++ ClientApp/src/app/shared-ui-components/product-selector-input/product-selector-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-selector-input',
+  templateUrl: './product-selector-input.component.html',
+  styleUrls: ['./product-selector-input.component.scss']
+})
+export class ProductSelectorInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
