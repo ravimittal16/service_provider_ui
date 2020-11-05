0ce9b09e1cde57e77fa5f1976d6c3d90b4b205ea
Ravi Mittal <ravimittal16@gmail.com>
2020-11-05 22:52:33 +0530

    Added edit page view
    Added edit page view

--- ClientApp/src/app/jobs/job-card-list-item/job-card-list-item.component.ts
+++ ClientApp/src/app/jobs/job-card-list-item/job-card-list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { JobDto } from "@shared/service-proxies/service-proxies";
 
 @Component({
@@ -9,7 +10,11 @@ import { JobDto } from "@shared/service-proxies/service-proxies";
 export class JobCardListItemComponent implements OnInit {
   @Input() job: JobDto;
   @Input() isFirstItem: boolean;
-  constructor() {}
+  constructor(private _router: Router) {}
+
+  onJobTitleClicked(): void {
+    this._router.navigate(["app/jobs/editJob", this.job.jobId]);
+  }
 
   ngOnInit(): void {}
 }
