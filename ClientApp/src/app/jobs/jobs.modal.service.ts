c1555c8fe4032b43d7d6337f6f343e30327c5ba4
Ravi Mittal <ravimittal16@gmail.com>
2020-11-06 11:06:08 +0530

    Updated Modal (Vertical)
    Updated Modal (Vertical)

--- ClientApp/src/app/jobs/jobs.modal.service.ts
+++ ClientApp/src/app/jobs/jobs.modal.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from "@angular/core";
 import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
+import { JobDto } from "@shared/service-proxies/service-proxies";
+import { BehaviorSubject } from "rxjs";
 
 import { AddJobModalComponent } from "./add-job-modal/add-job-modal.component";
-
+export class JobVerticalModalModel {
+  jobDto?: JobDto;
+  open: boolean;
+}
 @Injectable()
 export class JobsModalService {
+  private jobVerticalModalBehaviour: BehaviorSubject<
+    JobVerticalModalModel
+  > = new BehaviorSubject<JobVerticalModalModel>(null);
+  jobVerticalModalBehaviour$ = this.jobVerticalModalBehaviour.asObservable();
+
   constructor(private modalService: NgbModal) {}
+
+  openJobDetailModal(open: boolean, job?: JobDto) {
+    this.jobVerticalModalBehaviour.next({ jobDto: job, open: open });
+  }
 
   openCreateJobModal(): NgbModalRef {
     const modalRef = this.modalService.open(AddJobModalComponent, {
