5a77277cc13d36de9b03901a74a3794f4c3bec11
Ravi Mittal <ravimittal16@gmail.com>
2020-11-21 15:13:42 +0530

    Add Visit Component
    Add Visit Component

--- ClientApp/src/app/jobs/add-job-visit-modal/add-job-visit-modal.component.ts
+++ ClientApp/src/app/jobs/add-job-visit-modal/add-job-visit-modal.component.ts
@@ -11,12 +11,48 @@ import { JobDetailsDto } from "@shared/service-proxies/service-proxies";
 export class AddJobVisitModalComponent implements OnInit {
   @Input() job: JobDetailsDto;
   newVistFormGroup: FormGroup;
+  validationMessages: { [key: string]: string } = {};
   constructor(public activeModal: NgbActiveModal, private _fb: FormBuilder) {}
+  onStartDateChanged(newDate: Date) {}
+  getFieldValue(fieldName: string) {
+    return this.newVistFormGroup.get(fieldName).value;
+  }
+
+  onScheduleLaterCheckChange(): void {
+    const __scheduleLater = this.newVistFormGroup.get("scheduleLater")
+      .value as boolean;
+    this.newVistFormGroup.get("startDate").enable();
+    this.newVistFormGroup.get("startTime").enable();
+    this.newVistFormGroup.get("endDate").enable();
+    this.newVistFormGroup.get("endTime").enable();
+    if (__scheduleLater) {
+      this.newVistFormGroup.get("startDate").disable();
+      this.newVistFormGroup.get("startTime").disable();
+      this.newVistFormGroup.get("endDate").disable();
+      this.newVistFormGroup.get("endTime").disable();
+    }
+  }
+
   private _initNewVisitForm() {
+    const __defaultTitle = `${this.job?.customer?.displayName} - ${this.job.title}`;
+    const __now = new Date();
     this.newVistFormGroup = this._fb.group({
-      title: ["", [Validators.required]],
+      title: [__defaultTitle, [Validators.required, Validators.maxLength(200)]],
+      description: ["", []],
+      assignedTo: [null],
+      scheduleLater: [false],
+      startDate: [__now],
+      startTime: [__now],
+      endDate: [null],
+      endTime: [null],
     });
   }
+
+  onCancelClicked() {
+    const __dirty = this.newVistFormGroup.dirty;
+    this.activeModal.close();
+  }
+
   ngOnInit(): void {
     this._initNewVisitForm();
   }
